1a731e4f3fc2d31843db0c33c729189b5ca4ca79
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-09-29 17:36:43 +0600

    margin solved

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
 
     return (
         <div className='grid grid-rows-2 lg:grid-rows-1 grid-flow-row lg:grid-flow-col'>
-            <div className='row-span-4 m-0 lg:m-10 mb-0 lg:mb-0'>
+            <div className='row-span-4 m-2 lg:m-10 mb-0 lg:mb-0'>
 
                 <div className='flex items-center gap-2 mb-4'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 text-cyan-400 mt-3 sm:mt-3 md:mt-5   ">
@@ -45,7 +45,7 @@ const Home = () => {
                     }
                 </div>
             </div>
-            <div className='row-span-1 bg-white mt-3 sm:mt-3 lg:mt-0'>
+            <div className='row-span-1 bg-white mt-3 sm:mt-3 lg:mt-2 shadow-lg m-2 sm:m-2 lg:m-0 rounded-lg sm:rounded-lg lg:rounded-none'>
                 <Profile></Profile>
                 <Details></Details>
                 <Break gap={gap} setGap={setGap}></Break>
