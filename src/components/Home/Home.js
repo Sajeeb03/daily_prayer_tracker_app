65c8e306737634db013a8c0acd603bf4261aa54a
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-09-29 06:55:07 +0600

    home section addeded

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Prayers from '../Prayers/Prayers';
 
 const Home = () => {
     return (
-        <div>
-            <h2>hello from home</h2>
+        <div className='grid grid-rows-2 grid-flow-row lg:grid-flow-col'>
+            <div className='row-span-4 bg-slate-400'>
+                <Prayers></Prayers>
+            </div>
+            <div className='row-span-1 bg-red-600 lg:bg-red-800'>
+
+            </div>
         </div>
     );
 };
---
+++ src/components/Prayers/Prayers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Prayers = () => {
+    return (
+        <div className='bg-lime-400 border-red-400'>
+            hello
+        </div>
+    );
+};
+
+export default Prayers;
