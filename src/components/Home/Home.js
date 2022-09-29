101f7682b9c921fbadd81b156deb20706a14259c
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-09-29 08:35:17 +0600

    card-addeded

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,12 +10,17 @@ const Home = () => {
     }, [])
 
     return (
-        <div className='grid grid-rows-2 grid-flow-row lg:grid-flow-col'>
-            <div className='row-span-4 bg-slate-400'>
-                <Prayers></Prayers>
+        <div className='grid grid-rows-2 lg:grid-rows-1 grid-flow-row lg:grid-flow-col bg-slate-200'>
+            <div className='row-span-4 m-0 lg:m-10'>
+                <h1>Daily Prayer Tracker</h1>
+                <div className='grid grid-rows-2 gap-2 lg:grid-flow-col'>
+                    {
+                        prayers.map(prayer => <Prayers prayer={prayer} key={prayer.id}></Prayers>)
+                    }
+                </div>
             </div>
             <div className='row-span-1 bg-red-600 lg:bg-red-800'>
-
+                hello
             </div>
         </div>
     );
--- src/components/Prayers/Prayers.js
+++ src/components/Prayers/Prayers.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Prayers = () => {
+const Prayers = ({ prayer }) => {
+    const { name, details, img, duration } = prayer;
     return (
-        <div className='h-40'>
-            hello
+        <div className='border-2 bg-white rounded-lg h-[300px]'>
+            <div className='m-2'>
+                <img className='w-full h-40 rounded-lg' src={img} alt="" />
+                <h1 className='text-3xl font-semibold'>{name}</h1>
+                <p>{details}</p>
+                <p className='font-semibold'>Duration: {duration}minutes</p>
+            </div>
         </div>
     );
 };
