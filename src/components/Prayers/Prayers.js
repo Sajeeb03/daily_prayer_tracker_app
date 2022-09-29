1626f1b354a1b684c13111efa03dec7589943418
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-09-29 09:14:10 +0600

    btn added

--- src/components/Prayers/Prayers.js
+++ src/components/Prayers/Prayers.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Prayers = ({ prayer }) => {
     const { name, details, img, duration } = prayer;
     return (
-        <div className='border-2 bg-white rounded-lg h-[300px]'>
+        <div className='border-2 bg-white h-[360px] rounded-lg font-serif relative'>
             <div className='m-2'>
                 <img className='w-full h-40 rounded-lg' src={img} alt="" />
                 <h1 className='text-3xl font-semibold'>{name}</h1>
                 <p>{details}</p>
                 <p className='font-semibold'>Duration: {duration}minutes</p>
+                <button className='border-2 bg-cyan-400 rounded-lg p-2 mt-5 text-blue font-serif font-semibold absolute bottom-1'>Prayed Today</button>
             </div>
         </div>
     );
